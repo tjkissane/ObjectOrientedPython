7f9673de990a035c7dd34b04f500a58eb7ee06cf
Tom <tjkissane@outlook.com>
2024-05-11 21:26:04 -0500

    Initial commit of higher_or_lower_procedural.py

---
+++ higher_or_lower_procedural.py
@@ -0,0 +1,8 @@
+# Higher_or_lower
+
+import random
+
+# Card Constants
+SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
+RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+
