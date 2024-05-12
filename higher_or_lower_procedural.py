5d1953bd82fdd1cfb8e082e17d60a386f7dbc737
Tom <tjkissane@outlook.com>
2024-05-11 21:45:02 -0500

    Initial commit of higher_or_lower_procedural.py

--- higher_or_lower_procedural.py
+++ higher_or_lower_procedural.py
@@ -6,3 +6,9 @@ import random
 SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
 
+NCARDS = 8
+
+# Pass in a deck and this function returns a random card from the deck
+def getCard(deckListIn):
+    thisCard = deckListIn.pop() #pop off the top of the deck and return
+    return thisCard
